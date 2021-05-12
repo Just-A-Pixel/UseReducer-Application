759a1399a216c537678935253afc529b6bb9210c
Just-A-Pixel <Raj.Anand0511@gmail.com>
2021-05-12 21:18:02 +0530

    dawa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const ACTIONS = {
+  INCREMENT: 'increment',
+  DECREMENT: 'decrement'
 }
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTIONS.INCREMENT:
+      return { count: state.count + 1 };
+    case ACTIONS.DECREMENT:
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+  // const [count, setCount] = useState(0);
+
+  const increment = () => {
+    dispatch({ type: ACTIONS.INCREMENT });
+  };
+
+  const decrement = () => {
+    dispatch({ type: ACTIONS.DECREMENT });
+  };
+
+  return (
+    <>
+      <button onClick={decrement}>-</button>
+      <span>{state.count}</span>
+      <button onClick={increment}>+</button>
+    </>
+  );
+};
 
 export default App;
